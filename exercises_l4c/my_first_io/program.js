f37e1f7e5bd19fb598631b30986f169d19e9664d
E T <guofoo>
2015-04-17 22:37:58 +0000

    commit exercise content by Live4Code

--- exercises_l4c/my_first_io/program.js
+++ exercises_l4c/my_first_io/program.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 
 // enter your code below
-var contents = fs.readFieSync(process.argv[2])
+var contents = fs.readFileSync(process.argv[2])
 
 
 var lines = contents.toString().split('\n').length - 1
