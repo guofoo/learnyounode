5a4dccd336eaccc04cffeee1fbafb9dc9a244e24
E T <guofoo>
2015-05-23 00:59:41 +0000

    commit exercise content by Live4Code

--- exercises_l4c/http_client/program.js
+++ exercises_l4c/http_client/program.js
@@ -3,8 +3,12 @@ var http = require('http')
 http.get(process.argv[2], function (response) {
   response.setEncoding('utf8')
   // enter your code below
-//  response.on('data', console.log)
+  response.on('data', console.log)
 
   response.on('error', console.error);
 });
 
+// exit if no response after 1 second
+setTimeout(function() {
+  process.exit();
+}, 1000);
