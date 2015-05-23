506815e9cd3f6905e6bcbb538ba7c960ff89fa2a
E T <guofoo>
2015-05-23 01:07:01 +0000

    commit exercise content by Live4Code

--- exercises_l4c/http_collect/program.js
+++ exercises_l4c/http_collect/program.js
@@ -3,7 +3,7 @@ var bl = require('bl')
 
 http.get(process.argv[2], function (response) {
   // enter your code below
-  response.pipe(bl(function (err, data) {
+//  response.pipe(bl(function (err, data) {
 
     if (err)
       return console.error(err)
