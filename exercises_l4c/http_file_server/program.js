f2a5a4d1df02ae7df7cfe638f0c8b79214237986
E T <guofoo>
2015-05-28 23:11:12 +0000

    commit exercise content by Live4Code

--- exercises_l4c/http_file_server/program.js
+++ exercises_l4c/http_file_server/program.js
@@ -3,7 +3,9 @@ var fs = require('fs')
 
 var server = http.createServer(function (req, res) {
   // write your code below
-
+  res.writeHead(200, { 'content-type': 'text/plain' })
+  
+  fs.createReadStream(process.argv[3]).pipe(res)
 })
 
 server.listen(Number(process.argv[2]))
