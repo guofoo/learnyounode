4d3e1f12233c8ed9d38cac3383e14f43b3979173
E T <guofoo>
2015-06-18 17:52:01 +0000

    commit exercise content by Live4Code

--- exercises_l4c/http_client/program.js
+++ exercises_l4c/http_client/program.js
@@ -1,9 +1,9 @@
-var http = require('http')
+var http = require('http');
 
 http.get(process.argv[2], function (response) {
-  response.setEncoding('utf8')
+  response.setEncoding('utf8');
   // enter your code below
-  response.on('data', console.log)
+  response.on('data', console.log);
 
   response.on('error', console.error);
 });
